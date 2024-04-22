4cb2388c2d97052089b897b207be8f68b7439f8d
Matheus <gueffmatheus@gmail.com>
2024-04-22 20:42:33 -0300

    Realizado os exercícios da PROVA1

--- p02.py
+++ p02.py
@@ -1,11 +1,13 @@
-pop_a = 100
-pop_b = 200
+pop_a = 80000
+pop_b = 200000
 taxa_a = 1.3
 taxa_b = 1.15
 cont_anos = 0
 
+print(f"População A inicial: {pop_a}")
+print(f"População B inicial: {pop_b}")
+print("=====")
 while pop_a <= pop_b:
-
     pop_a = pop_a * taxa_a
     pop_b = pop_b * taxa_b
     cont_anos += 1
@@ -13,14 +15,14 @@ while pop_a <= pop_b:
         print(f"{cont_anos} ano se passou")
     else:
         print(f"{cont_anos} anos se passaram")
-    print(f"População A atual: {pop_a:.2f}")
-    print(f"População B atual: {pop_b:.2f}")
+    print(f"População A atual: {int(pop_a)}")
+    print(f"População B atual: {int(pop_b)}")
     print("=====")
 
 if(cont_anos == 1):
-    print(f"Foi necessário {cont_anos} ano até a população A se igualar ou passar a população B")
+    print(f"Foi necessário {cont_anos} ano até a população A({int(pop_a)} habitantes) passar ou se igualar a população B({int(pop_b)} habitantes)")
 else:
     if(pop_a == pop_b):
-        print(f"Foram necessários {cont_anos} anos até a população A se igualar a população B")
+        print(f"Foram necessários {cont_anos} anos até a população A({int(pop_a)} habitantes) passar a população B({int(pop_b)} habitantes)")
     else:
-        print(f"Foram necessários {cont_anos} anos até a população A({int(pop_a)}) passar a população B({int(pop_b)})")
+        print(f"Foram necessários {cont_anos} anos até a população A({int(pop_a)} habitantes) passar a população B({int(pop_b)} habitantes)")
